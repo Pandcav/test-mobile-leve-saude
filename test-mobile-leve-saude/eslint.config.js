cfdbcb906ced9be976bb74b833c0a3763c3e6b16
Pandcav <moiseslima47@gmail.com>
2025-07-28 21:56:05 -0300

    implementing configurations

---
+++ test-mobile-leve-saude/eslint.config.js
@@ -0,0 +1,58 @@
+import js from '@eslint/js';
+import reactNative from '@react-native-community/eslint-config';
+import typescript from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
+import prettier from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+        global: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+        module: 'readonly',
+        require: 'readonly',
+        exports: 'readonly',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': typescript,
+      prettier: prettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'no-unused-vars': 'off', // Use TypeScript rule instead
+    },
+  },
+  {
+    ignores: [
+      'node_modules/',
+      '.expo/',
+      'dist/',
+      '.expo-shared/',
+      '*.log',
+      'babel.config.js',
+      'metro.config.js',
+      'jest.config.js',
+    ],
+  },
+  prettierConfig,
+];
